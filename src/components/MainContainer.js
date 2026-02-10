3b5a845f49e4fb1c2a5ff16cf13684df8eb66cd7
vijaydurgasi <vijayanu292157@gmail.com>
2026-02-10 17:41:21 +0530

    used API for movies

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useSelector } from "react-redux"
+import VideoTitle from './VideoTitle'
+import VideoBackground from './VideoBackground'
 
 const MainContainer = () => {
+
+    const movies = useSelector(store => store.movies?.nowPlayingMovies);
+    if (movies === null) return;
+
+    const mainmovie = movies[0];
+    console.log("main-movie", mainmovie);
     return (
-        <div>MainContainer</div>
+        <div>
+            <VideoTitle />
+            <VideoBackground />
+        </div>
+
     )
 }
 
---
+++ src/components/VideoBackground.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function VideoBackground() {
+    return (
+        <div>VideoBackground</div>
+    )
+}
+
+export default VideoBackground
